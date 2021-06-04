987134e6f536e4a0172d5fd143963c7fa9b44867
CristianX <thecristianx@hotmail.com>
2021-06-04 11:13:29 -0500

    Mostrando campos de la bdd en la pantallad e usuarios

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -2,14 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Usuario;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class UsuarioController extends Controller
 {
     public function index() {
-        // $usuarios = DB::table('usuarios')->get();
-        // dd($usuarios);
-        return view('usuario.index');
+        // $usuarios = Usuario::all();
+        // // dd($usuarios);
+        return view('usuario.index')->with([
+            'usuarios' => Usuario::all(),
+        ]);
+    }
+
+    public function create() {
+        //TODO: crear post
     }
 }
--- resources/views/usuario/index.blade.php
+++ resources/views/usuario/index.blade.php
@@ -46,8 +46,37 @@
             <button type="submit" class="btn btn-primary">Registrar</button>
           </form>
           <table>
-              <thead>Datos de Usuarios</thead>
-              <tbody></tbody>
+              <thead class="thead-light">
+                <tr>
+                    <th>Identificación</th>
+                    <th>Nombres</th>
+                    <th>Apellidos</th>
+                    <th>Edad</th>
+                    <th>Correo Electrónico</th>
+                    <th>Teléfono Convencional</th>
+                    <th>Teléfono Celular</th>
+                    <th>Nacionalidad</th>
+                    <th>Parroquia de Residencia</th>
+                    <th>Barrio de Residencia</th>
+                </tr> 
+              </thead>
+              <tbody>
+                  @foreach ($usuarios as $usuario)
+                        <tr>
+                            <td>{{ $usuario->IDENTIFICACION_USUARIO }}</td>
+                            <td>{{ $usuario->NOMBRES_USUARIO }}</td>
+                            <td>{{ $usuario->APELLIDOS_USUARIO }}</td>
+                            <td>{{ $usuario->EDAD_USUARIO }}</td>
+                            <td>{{ $usuario->EMAIL_USUARIO }}</td>
+                            <td>{{ $usuario->TELFCONVENCIONAL_USUARIO }}</td>
+                            <td>{{ $usuario->TELFCELULAR_USUARIO }}</td>
+                            <td>{{ $usuario->NACIONALIDAD_USUARIO }}</td>
+                            <td>{{ $usuario->PARROQUIARES_USUARIO }}</td>
+                            <td>{{ $usuario->BARRIORES_USUARIO }}</td>
+                        </tr>
+                  @endforeach
+                  
+              </tbody>
           </table>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
     </body>
