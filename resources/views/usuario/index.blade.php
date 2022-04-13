bfde8d5eac5df640d855e09f46c7c1ea22ae9b5c
CristianX <thecristianx@hotmail.com>
2022-04-13 15:35:47 -0500

    Adaptación parcial de pantalla de registro y pantalla de inicio

--- resources/views/usuario/index.blade.php
+++ resources/views/usuario/index.blade.php
@@ -32,7 +32,96 @@
         @if (session('error'))
             <div class="alert alert-danger">{{ session('error') }}</div>
         @endif
-        <div class="container">
+
+        <div class="container d-flex justify-content-center align-items-center" style="vertical-align: middle">
+            <div class="card" style="background-color: bisque; width: 75%;">
+                <div class="content-box" style="background-color: #4CBBCE; color: white; width: 100%; text-align: center; padding-top: 10px; padding-bottom: 10px; ">
+                    <h3 style="font-weight: bold">Ingresa tus Datos</h3>
+                </div>
+                <form method="POST" action="{{ route('usuario.store') }}" style="padding: 10px" >
+                    @csrf
+                    <div class="row">
+                        <div class="col">
+                            <div class="mb-3">
+                                <label for="nombres" class="form-label">Nombres</label>
+                                <input placeholder="Ingrese sus Nombres" value="{{ $primerNombre }} {{ $segundoNombre }}" type="text" class="form-control" name="NOMBRES_USUARIO" minlength="4" maxlength="200" required readonly>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="mb-3">
+                                <label for="apellidos" class="form-label">Apellidos</label>
+                                <input placeholder="Ingrese sus Apellidos" value="{{ $apellidoPaterno }} {{ $apellidoMaterno }}" type="text" class="form-control" name="APELLIDOS_USUARIO" minlength="4" maxlength="200" required readonly>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="mb-3">
+                                <label for="identificacion" class="form-label">C.I./Pasaporte</label>
+                                <input placeholder="Ingrese su identificación" value="{{ $cedula }}" type="text" pattern="\d*" class="form-control"name="IDENTIFICACION_USUARIO" maxlength="15" minlength="5" required readonly>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="mb-3">
+                                <label for="FECHANACIMIENTO_USUARIO" class="form-label">Fecha de Nacimiento</label>
+                                <input type="date" value="{{ date('Y-m-d', strtotime($fechaNacimiento)) }}" class="form-control" id="txtDate" name="FECHANACIMIENTO_USUARIO" required readonly>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="mb-3">
+                                <label for="nacionalidad" class="form-label">Nacionalidad</label>
+                                <input placeholder="Ingrese su Nacionalidad" value="{{ $nacionalidad }}" type="text" class="form-control" name="NACIONALIDAD_USUARIO" minlength="4" maxlength="200" required readonly>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col">
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Correo Electrónico</label>
+                                <input placeholder="Ingrese su Correo Electrónico" type="email" class="form-control" name="EMAIL_USUARIO" required>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="mb-3">
+                                <label for="telefCelular" class="form-label">Teléfono Celular</label>
+                                <input placeholder="Ingrese su teléfono celular" type="tel" pattern="\d*" class="form-control" minlength="10" maxlength="10" name="TELFCELULAR_USUARIO" required>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="mb-3">
+                                <label for="telefConvencional" class="form-label">Teléfono de Emergencia</label>
+                                <input placeholder="Ingrese su teléfono convencional" type="tel" pattern="\d*" class="form-control" minlength="7" maxlength="10" name="TELFCONVENCIONAL_USUARIO" required>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="mb-3">
+                                <label for="PARROQUIARES_USUARIO" class="form-label">Parroquia donde Reside</label>
+                                <select class="form-select" name="PARROQUIARES_USUARIO" required>
+                                    <option value="" selected disabled>Seleccione una Parróquia</option>
+                                    @foreach ($parroquias as $parroquia)
+                                        <option value="{{$parroquia}}">{{$parroquia}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="mb-3">
+                                <label for="barrioDondeReside" class="form-label">Barrio donde Reside</label>
+                                <input placeholder="Ingrese el Barrio donde Reside" type="text" class="form-control" name="BARRIORES_USUARIO" minlength="4" maxlength="600" required>
+                            </div>
+                        </div>
+                        <div class="col" style="vertical-align: bottom">
+                            <button type="submit" class="btn btn-primary estiloBoton">Registrar</button>
+                        </div>
+                    </div>
+                </form> 
+            </div>
+        </div>
+
+        {{-- <div class="container">
             <form method="POST" action="{{ route('usuario.store') }}" style="padding: 10px" >
                 @csrf
                 <div class="mb-3">
@@ -46,12 +135,13 @@
                 <div class="mb-3">
                     <label for="apellidos" class="form-label">Apellidos</label>
                     <input placeholder="Ingrese sus Apellidos" value="{{ $apellidoPaterno }} {{ $apellidoMaterno }}" type="text" class="form-control" name="APELLIDOS_USUARIO" minlength="4" maxlength="200" required readonly>
-                </div>
-                {{-- <div class="mb-3">
-                    <label for="edad" class="form-label">Edad</label>
-                    <input placeholder="Ingrese su Edad" type="number" min="18" max="120" step="1" class="form-control" name="EDAD_USUARIO" required>
                 </div> --}}
-                <div class="mb-3">
+                                                    {{-- Esto es o no es sho que se --}}
+                                                        {{-- <div class="mb-3">
+                                                            <label for="edad" class="form-label">Edad</label>
+                                                            <input placeholder="Ingrese su Edad" type="number" min="18" max="120" step="1" class="form-control" name="EDAD_USUARIO" required>
+                                                        </div> --}}
+                {{-- <div class="mb-3">
                     <label for="FECHANACIMIENTO_USUARIO" class="form-label">Fecha de Nacimiento</label>
                     <input type="date" value="{{ date('Y-m-d', strtotime($fechaNacimiento)) }}" class="form-control" id="txtDate" name="FECHANACIMIENTO_USUARIO" required readonly>
                 </div>
@@ -86,13 +176,19 @@
                 </div>
                 <button type="submit" class="btn btn-primary estiloBoton">Registrar</button>
             </form>
-        </div>
+        </div> --}}
         
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
     </body>
 </html>
 
 <style class="text/css">
+    .abs-center {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+    }
     .contenedorIzquierda {
         justify-content: left;
     }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,31 +28,42 @@
         </div>
         <div class="container">
             <div class="row">
-                <div class="col-sm-7 imagen">
-                <img src="{{ asset('/assets/Bicentenario.png') }}" style="width: 90%">
+                <div class="col-sm-8 imagen">
+                <img src="{{ asset('/assets/izquierda_inicio.svg') }}" style="width: 60%">
                 </div>
-                <div class="col-sm-5 formulario">
+                <div class="col-sm-4 formulario">
                     <div class="card cardFormulario">
                         <div class="content-box" style="background-color: #4CBBCE; color: white; width: 100%; text-align: center; padding-top: 10px; padding-bottom: 10px; ">
-                            <h3 style="font-weight: bold">Registro de bicicletas</h3>
+                            <h4 style="font-weight: bold">Registro de bicicletas</h4>
                         </div>
                         <div class="card-body">
                             <div class="form-group" style="text-align: center;">
-                                <i class="fas fa-id-card fa-2x" style="color: #4CBBCE; padding: 10px; vertical-align: middle"></i>
-                                <label style="text-align: start; te;" for="notificacion" class="form-label">
-                                    Ingresa tu C.I. o Pasaporte
-                                </label>
-                                <br>
+                                <div class="row align-items-center" style="padding: 5px">
+                                    <div class="col-sm-2">
+                                        <i class="fas fa-id-card fa-2x" style="color: #4CBBCE"></i>
+                                    </div>
+                                    <div class="col-sm-10" style="text-align: left">
+                                        <label for="notificacion" class="form-label">
+                                            Ingresa tu C.I. o Pasaporte
+                                        </label>
+                                    </div>
+                                </div>
                                 <form method="GET" action="{{ route('welcome.pasarRegBIcicletas') }}">
-                                    <div class="form-floating mb-3">
+                                    <div class="form-floating mb-3 ">
                                         <input name="identificacion" style="width: 100%;" id="identificacion" placeholder="Ingrese su Identificación" type="text" pattern="\d*" class="form-control" autocomplete="off" maxlength="15" minlength="5" required>
                                         <label for="identificacion" style="color: #bdbdbd; margin-left: 5%">C.I./Pasaporte</label>
                                     </div>
                                     <button class="btn btn-primary" type="submit" >Registrate/Ingresa</button>
                                 </form>
                                 <br>
-                                <i class="fas fa-barcode fa-2x" style="color: #4CBBCE; padding: 10px; vertical-align: middle"></i>
-                                <label style="text-align: start" for="notificacionCodRegistro" class="form-label">Consulta por Código de Registro</label>
+                                <div class="row align-items-center">
+                                    <div class="col-sm-2">
+                                        <i class="fas fa-barcode fa-2x" style="color: #4CBBCE"></i>
+                                    </div>
+                                    <div class="col-sm-10" style="text-align: left">
+                                        <label for="notificacionCodRegistro" class="form-label">Consulta por Código<br/>de registro</label>
+                                    </div>
+                                </div>
                                 {{-- <label style="text-align: start" for="notificacionCodRegistro" class="form-label">o Nº de Serie</label> --}}
                                 <form method="GET" action="{{ route('welcome.consulta') }}">
                                     <div class="form-floating mb-3">
@@ -82,26 +93,27 @@
 
 <style type="text/css">
     .imagen {
-        padding-top: 12%;
+        padding-top: 15%;
+        text-align: left;
     }
     .formulario {
         
-        padding-top: 6%;
+        padding-top: 3%;
         padding-right: 4%;
     }
     .cardFormulario {
         /* display: flex; */
         justify-content: center;
         align-items: center;
-        border-radius: 10%;
+        border-radius: 25px;
         /* box-shadow: 5px 5px 10px black; */
-        width: 80%;
+        width: 100%;
         height: 100%;
         /* background-color: red; */
         /* background-image:url("{{ asset('/assets/wheel.png') }}"); */
         background-size: contain;
         overflow: hidden;
-        background-color: rgba(255, 255, 255, 0.6);
+        background-color: rgba(255, 255, 255, 0.75);
     }
     /* .background_image {
         position: fixed;
@@ -112,10 +124,11 @@
         background-image: url('/assets/Fondo_1.png');
     } */
     .background_image {
-        background-image: url("{{ asset('/assets/iconografia.png') }}");
-        /* background-color: red; */
-        background-repeat: no-repeat;
-        background-position: bottom right;
-        background-size: 80%;
+        /* position: fixed; */
+        /* left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%; */
+        background-image: url('/assets/Fondo_1.png');
     }
 </style>
